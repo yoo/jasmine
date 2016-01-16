ffb868f3f1e40f813d3ee59823060afce33fca13
arvitaly <arvitaly@list.ru>
2016-01-16 23:16:26 +0300

    Fix and add async functions with tests, add Fail function, fix travis

--- jasmine.go
+++ jasmine.go
@@ -24,21 +24,48 @@ func XIt(behavior string, fn func()) {
 }
 
 func ItAsync(behavior string, fn func(func())) {
-	js.Global.Call("it", behavior, fn)
+
+	var cb = js.MakeFunc(func(this *js.Object, args []*js.Object) interface{} {
+		return js.Global.Get("eval").Invoke("var a = function(cb){ return function(done){ return cb(done); }; }; a")
+	}).Invoke().Invoke(fn)
+	js.Global.Call("it", behavior, cb)
 }
 
 func XitAsync(behavior string, fn func(func())) {
-	js.Global.Call("xit", behavior, fn)
+
+	js.Global.Call("xit", behavior, createFuncWithVisibleDone(fn))
 }
 
 func BeforeEach(fn func()) {
 	js.Global.Call("beforeEach", fn)
 }
 
+func BeforeEachAsync(fn func(func())) {
+	js.Global.Call("beforeEach", createFuncWithVisibleDone(fn))
+}
+
 func AfterEach(fn func()) {
 	js.Global.Call("afterEach", fn)
 }
 
+func AfterEachAsync(fn func(func())) {
+	js.Global.Call("afterEach", createFuncWithVisibleDone(fn))
+}
+
 func Expect(value interface{}) *Expectation {
 	return &Expectation{o: js.Global.Call("expect", value)}
+}
+
+func SetDefaultTimeoutInterval(interval int) {
+	js.Global.Get("jasmine").Set("DEFAULT_TIMEOUT_INTERVAL", interval)
+}
+
+func Fail(message string) {
+	js.Global.Call("fail", message)
+}
+
+func createFuncWithVisibleDone(fn func(func())) *js.Object {
+	return js.MakeFunc(func(this *js.Object, args []*js.Object) interface{} {
+		return js.Global.Get("eval").Invoke("var a = function(cb){ return function(done){ return cb(done); }; }; a")
+	}).Invoke().Invoke(fn)
 }
